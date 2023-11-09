ddb042a24746dc6e605a41f43d45a695eb06e37a
navahok164 <s3997024@rmit.edu.vn>
2023-11-09 17:41:14 +0700

    final tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{js,jsx,ts,tsx}",
-  ,"./public/**/*.{js,jsx,ts,tsx}"],
+  ,"./public/**/*.{js,jsx,ts,tsx}",
+  "./public/**/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
