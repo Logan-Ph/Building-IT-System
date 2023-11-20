d292008e14e55659b0e88f82772bfb58ced8b3e0
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2023-11-20 23:33:48 +0700

    mai update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: [
+    require('flowbite/plugin')
+  ],
+
   content: ["./src/**/*.{js,jsx,ts,tsx}",
   ,"./src/**/*.{js,jsx,ts,tsx}",
-  "./src/**/**/*.{js,jsx,ts,tsx}"],
+  "./src/**/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
@@ -16,6 +20,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+ 
 }
 
