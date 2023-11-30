a43f6962a21dbe1f391989a5081da46ed8b3bdad
navahok164 <s3997024@rmit.edu.vn>
2023-11-30 16:46:07 +0700

    a

--- client/src/Pages/DashboardPage.js
+++ client/src/Pages/DashboardPage.js
@@ -1,6 +1,7 @@
 import Sidebar from "../Components/Sidebar";
 import BarChart from '../Components/BarChart';
 import Insight from "../Components/Insight";
+import ToDoList from "../Components/ToDoList";
 
 export default function DashboardPage() {
   return (
@@ -15,15 +16,16 @@ export default function DashboardPage() {
 
         <div class=" bg-white h-auto w-full px-20  text-slate-300 relative py-4 ">
           <div id="content" class="bg-slate-200 col-span-9 rounded-lg p-6 mx-auto px-auto mb-12">
-          <h1 class="font-bold  p-4 uppercase text-black text-2xl">To Do List</h1>
-            <Insight />
+          <h1 class="font-bold  pl-5 pt-4 uppercase text-black text-2xl">To Do List</h1>
+          <h1 class="font-medium  pt-1 pl-5 text-gray-500 text-md">Things your business need to deal with</h1>
+            <ToDoList />
             </div>
           <div class="">
             <div id="content" class="bg-slate-200 col-span-9 rounded-lg p-6 mx-auto px-auto">
-            <h1 class="font-bold  p-4 uppercase text-black text-2xl">Business Insight</h1>
+            <h1 class="font-bold  pl-5 pt-4 uppercase text-black text-2xl">Business Insight</h1>
             <Insight />
               <div id="last-incomes">
-                <h1 class="font-bold text-xl  text-black uppercase p-4">Last Month Incomes</h1>
+                <h1 class="font-bold text-xl  text-black uppercase p-5">Last Month Incomes</h1>
                 <BarChart />
               </div>
               <div id="last-users">
