696fb9b87c28ea17cb7b4da4d66d47a5cf10a26c
navahok164 <s3997024@rmit.edu.vn>
2023-11-30 17:06:45 +0700

    complete to do list

--- client/src/Components/ToDoList.js
+++ client/src/Components/ToDoList.js
@@ -8,89 +8,80 @@ export default function ToDoList() {
                     <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
                       <div class="flex-auto p-4">
                         <div class="flex flex-wrap">
-                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-gray-400 uppercase font-bold text-xs">
+                          <div class="relative w-full pr-4 max-w-full flex flex-col items-center">
+                          <span class="font-bold text-xl text-blue-700">
+                              334,100
+                            </span>
+                            <h5 class="text-black uppercase font-bold text-xs">
                               {" "}
                               Unpaid
                             </h5>
-                            <span class="font-semibold text-xl text-gray-700">
+                            
+                          </div>
+                       
+                        </div>
+                       
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
+                      <div class="flex-auto p-4">
+                        <div class="flex flex-wrap">
+                          <div class="relative w-full pr-4 max-w-full flex flex-col items-center">
+                          <span class="font-bold text-xl text-blue-700">
                               334,100
                             </span>
-                          </div>
-                          <div class="relative w-auto pl-4 flex-initial">
-                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
-                              <i class="fas fa-chart-bar"></i>
-                            </div>
-                          </div>
-                        </div>
-                       
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class=" mt-4 w-full lg:w-6/12 xl:w-3/12 px-5">
-                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-4 xl:mb-0 shadow-lg">
-                      <div class="flex-auto p-4">
-                        <div class="flex flex-wrap">
-                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-gray-400 uppercase font-bold text-xs">
-                              To-Process Shipment 
-                            </h5>
-                            <span class="font-semibold text-xl text-gray-700">
-                              2,999
-                            </span>
-                          </div>
-                          <div class="relative w-auto pl-4 flex-initial">
-                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-pink-500">
-                            <i class="fa-solid fa-money-bill-wave"></i>                            </div>
-                          </div>
-                        </div>
-                      
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5">
-                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
-                      <div class="flex-auto p-4">
-                        <div class="flex flex-wrap">
-                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-gray-400 uppercase font-bold text-xs">
-                              Process Shipment
-                            </h5>
-                            <span class="font-semibold text-xl text-gray-700">
-                              901
-                            </span>
-                          </div>
-                          <div class="relative w-auto pl-4 flex-initial">
-                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-red-500">
-                              <i class="fas fa-users"></i>
-                            </div>
-                          </div>
-                        </div>
-                       
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5">
-                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
-                      <div class="flex-auto p-4">
-                        <div class="flex flex-wrap">
-                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-gray-400 uppercase font-bold text-xs">
-                            Pending Cancellation
+                            <h5 class="text-black uppercase font-bold text-xs">
+                              {" "}
+                              TO-PROCESS SHIPMENT 
                             </h5>
                             
-                            <span class="font-semibold text-xl text-gray-700">
-                              51.02%{" "}
+                          </div>
+                       
+                        </div>
+                       
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
+                      <div class="flex-auto p-4">
+                        <div class="flex flex-wrap">
+                          <div class="relative w-full pr-4 max-w-full flex flex-col items-center">
+                          <span class="font-bold text-xl text-blue-700">
+                              334,100
                             </span>
+                            <h5 class="text-black uppercase font-bold text-xs">
+                              {" "}
+                              PROCESS SHIPMENT
+                            </h5>
+                            
                           </div>
-                          <div class="relative w-auto pl-4 flex-initial">
-                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-emerald-500">
-                              <i class="fas fa-percent"></i>
-                            </div>
+                       
+                        </div>
+                       
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
+                      <div class="flex-auto p-4">
+                        <div class="flex flex-wrap">
+                          <div class="relative w-full pr-4 max-w-full flex flex-col items-center">
+                          <span class="font-bold text-xl text-blue-700">
+                              334,100
+                            </span>
+                            <h5 class="text-black uppercase font-bold text-xs">
+                              {" "}
+                              PENDING CANCELLATION
+                            </h5>
+                            
                           </div>
+                       
                         </div>
                        
                       </div>
@@ -101,26 +92,23 @@ export default function ToDoList() {
 
            
 
-                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5">
-                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+                  <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
+                    <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-3 xl:mb-0 shadow-lg">
                       <div class="flex-auto p-4">
                         <div class="flex flex-wrap">
-                          <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-gray-400 uppercase font-bold text-xs">
-                            Sold Out Products
+                          <div class="relative w-full pr-4 max-w-full flex flex-col items-center">
+                          <span class="font-bold text-xl text-blue-700">
+                              334,100
+                            </span>
+                            <h5 class="text-black uppercase font-bold text-xs">
+                              {" "}
+                              SOLD OUT PRODUCTS
                             </h5>
                             
-                            <span class="font-semibold text-xl text-gray-700">
-                              51.02%{" "}
-                            </span>
                           </div>
-                          <div class="relative w-auto pl-4 flex-initial">
-                            <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-emerald-500">
-                              <i class="fas fa-percent"></i>
-                            </div>
-                          </div>
+                       
                         </div>
-                      
+                       
                       </div>
                     </div>
                   </div>
