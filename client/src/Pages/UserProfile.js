dc0ca556b0764630c7ed4acbe98c38ef4b08de5f
ninnh <phoanninhninh@gmail.com>
2023-12-03 23:29:36 +0700

    Modify the UserProfile

--- client/src/Pages/UserProfile.js
+++ client/src/Pages/UserProfile.js
@@ -121,14 +121,11 @@ export default function UserProfile() {
                                             <label for="photo" className="block text-sm font-medium leading-6 text-gray-900">Avatar picture</label>
                                             <div className="mt-2 flex items-center gap-x-3">
                                                 <input type="file" id="fileUpload" name="photo" accept="image/*" />
-                                                <svg onclick="document.getElementById('fileUpload').click()" className="h-12 w-12 text-gray-300 cursor-pointer" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                                    <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-                                                </svg>
-                                                <button type="button" onclick="document.getElementById('fileUpload').click()" className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Change</button>
+                                               
                                             </div>
                                         </div>
 
-                                        <div className="sm:col-span-3">
+                                        <div className="sm:col-span-4">
                                             <label for="username" className="block text-sm font-medium leading-6 text-gray-900">Username</label>
                                             <div className="mt-2">
                                                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
