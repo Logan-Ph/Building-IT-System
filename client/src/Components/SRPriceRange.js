40e9a1bebfc1e21c6b3b9a46cb7a9a8bfedfebf0
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2023-12-14 00:28:08 +0700

    Search Result Page update

--- client/src/Components/SRPriceRange.js
+++ client/src/Components/SRPriceRange.js
@@ -1,6 +1,7 @@
 import Slider from 'react-slider'
 import { useEffect, useState } from 'react'
 import { useRange } from 'react-instantsearch';
+import { GoDash } from "react-icons/go";
 const MIN = 0;
 const MAX = 12000;
 
@@ -21,26 +22,45 @@ export default function SRPriceRange() {
   };
 
   return (
-    <div className='flex px-6'>
+    <div className='flex px-6 border border-gray-50 py-6 bg-white'>
       <div className='w-full'>
-        <h3 className='font-medium mb-2 mt-3'>Price <span className='font-medium'>Range</span></h3>
-        <div className={"values"}>${values[0]}- ${values[1]}</div>
-        <small className='font-light mt-2 block text-gray-500'>
+        <h3 className='font-medium mb-2'>Price <span className='font-medium'>Range</span></h3>
+        {/* <div className={"values"}>${values[0]}- ${values[1]}</div> */}
+        {/* <small className='font-light mt-2 block text-gray-500'>
           Current Range: ${values[1] - values[0]}
-        </small>
+        </small> */}
 
-        <Slider className={"slider"}
+        {/* <Slider className={"slider"}
           onChange={handleSliderChange}
           onAfterChange={handleAfterChange}
           value={values}
           min={MIN}
-          max={MAX} />
-          
-          
-        <form class="max-w-sm mx-auto">
-            <label for="number-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a number:</label>
-            <input type="number" id="number-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="90210" required/>
-        </form>
+          max={MAX} /> */}
+        
+        {/* New Price Range Filter */}
+        <div className='flex justify-center items-center'>
+
+          <div className='flex items-center'>
+            <i className="fa-solid fa-dollar-sign mr-2"></i>
+            <input type="number" id="number-input" 
+                    aria-describedby="helper-text-explanation" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-2xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2" placeholder="FROM" required/>
+          </div>
+          <div className='mx-2'>
+            <GoDash />
+          </div>
+          <div className='flex items-center'>
+            <i className="fa-solid fa-dollar-sign mr-2"></i>
+            <input type="number" id="number-input" 
+                    aria-describedby="helper-text-explanation" 
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-2xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2" placeholder="TO" required/>
+          </div>
+        </div> 
+        <div>
+          <button type="button" class="focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium text-sm rounded-md px-6 py-1 mt-4 mx-auto w-full">Apply Now</button> 
+        </div>
+        
+
 
       </div>
     </div>
--- client/src/Pages/User/SearchResultPage.js
+++ client/src/Pages/User/SearchResultPage.js
@@ -7,7 +7,7 @@ import SRProductCard from '../../Components/SRProductCard'
 import SRPagination from '../../Components/SRPagination'
 import SRPriceRange from '../../Components/SRPriceRange'
 import SRStarRating from '../../Components/SRStarRating'
-import SRSideBar from '../../Components/SRSideBar'
+
 import { useHits, useRefinementList } from 'react-instantsearch';
 import { Navigate, useParams } from 'react-router-dom';
 import SortOptions from '../../Components/SortOptions';
@@ -61,61 +61,21 @@ export default function Example() {
     return <div>Loading....</div>
   }
 
+  function YourComponent() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  
+  const handleToggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="bg-white">
+    <div className="bg-gray-100">
       {user && user.role === "Vendor" && <Navigate to={'/dashboard'} replace />}
       {user && user.role === "Admin" && <Navigate to={'/admin/manage-user'} replace />}
 
       <div>
         {/* Mobile filter dialog */}
-        <Transition.Root show={mobileFiltersOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-40 " onClose={setMobileFiltersOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
 
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
-                  <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">Filters</h2>
-                    <button
-                      type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
-                      onClick={() => setMobileFiltersOpen(false)}
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-
-                  {/* Filters */}
-                  <span className="mt-4 border-t border-gray-200">
-                    <SRPriceRange className="pt-6 px-4" />
-                    <SRStarRating className="pt-6 px-4" />
-                    <CheckboxLabel setValueFilter={setValueFilter} valueFilter={valueFilter} refine={refine} oldCategoryRef={oldCategoryRef} />
-                  </span>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition.Root>
 
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-10">
@@ -136,21 +96,20 @@ export default function Example() {
           </div>
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
-            <h2 id="products-heading" className="sr-only">
-              Products
-            </h2>
-
-            <div className="flex">
-              <div className='w-[35%] md:pr-16 lg:pr-20'>
+            <div className="xl:justify-start xl:divide-x-2 xl:gap-6 xl:flex lg:flex lg:justify-start lg:divide-x-2 lg:gap-6 md:flex sm:block ">
+              <div className='w-full'>
                 {/* Filters */}
-                <div className="xs:hidden sm:hidden lg:block wi">
+                <div className="xs:hidden sm:hidden md:block lg:block wi">
                   <SRPriceRange />
                   <SRStarRating />
                   <CheckboxLabel setValueFilter={setValueFilter} valueFilter={valueFilter} refine={refine} oldCategoryRef={oldCategoryRef} />
+                  <div className='flex px-6 border border-gray-50 pb-4 bg-white'>
+                    <button type="button" class="focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium text-sm rounded-md px-6 py-1 mt-4 mx-auto w-full">Reset</button> 
+                  </div>
                 </div>
               </div>
 
-              <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8'>
+              <div className='grid xs:grid-cols-1 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8'>
                 {/* Product grid */}
                 {hits.map(hit => (
                   <SRProductCard hit={hit} />
@@ -160,28 +119,8 @@ export default function Example() {
             <SRPagination />
           </section>
 
-          <section className='flex divide-y-2'>
-            <div className='border border-gray-900 bg-gray-100 divide-y-2'>
-              <div className=' bg-gray-100 border-b-white pr-4 py-4'>
-                  <div className='ps-0'>
-                    <CheckboxLabel setValueFilter={setValueFilter} valueFilter={valueFilter} refine={refine}/>
-                  </div>
-              </div>
-              <div className=' bg-gray-100 border-b-white pr-4 py-4'>
-                  <div className='ps-0'>
-                    <SRPriceRange />
-                  </div>
-              </div>
-            </div>
-            <div className='border border-black'></div>
-
-          </section>
         </main>
       </div>
-      
-                    
-
-
 
     </div>
   )
@@ -195,17 +134,18 @@ function CheckboxLabel({ setValueFilter, valueFilter, refine, oldCategoryRef }) 
   }
 
   return (
-    <div className='flex px-6'>
+    <div className='flex px-6 border border-gray-50 py-6 bg-white'>
       <div className='w-full'>
-        <h3 className='font-medium mb-2 mt-3'>Categories</h3>
+        <h3 className='font-medium mb-2'>Categories</h3>
         {valueFilter.map(option => (
           <div className='mb-2 flex items-center'>
             <input type="checkbox" className='rounded-md' onClick={() => handleItemClick(option.value)} checked={option.isRefined} />
-            <span className='ml-2 text-md'>{option.label}</span>
+            <span className='ml-2 text-sm'>{option.label}</span>
           </div>
         ))}
       </div>
     </div>
+
   );
 }
 
