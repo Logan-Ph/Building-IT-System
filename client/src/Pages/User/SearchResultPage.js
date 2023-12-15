d8d62a6b40a4bacacef5e5f35df580b1507c2765
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2023-12-15 20:32:12 +0700

    Mai update

--- client/src/Pages/User/SearchResultPage.js
+++ client/src/Pages/User/SearchResultPage.js
@@ -1,5 +1,7 @@
 import { Fragment, useContext, useEffect, useRef, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
+
+import { Dialog, Transition } from '@headlessui/react';
+
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { FunnelIcon } from '@heroicons/react/20/solid'
 import '../../css/searchresult.css';
@@ -61,6 +63,13 @@ export default function Example() {
     return <div>Loading....</div>
   }
 
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const handleToggleSidebar = () => {
+    setMobileFiltersOpen(!mobileFiltersOpen);
+  };
+
+
+
 
 
   return (
@@ -70,8 +79,6 @@ export default function Example() {
 
       <div>
         {/* Mobile filter dialog */}
-
-
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-10">
             <h1 className="text-3xl font-semibold tracking-tight text-gray-900">Search result for <span className='text-[#E61E2A] font-light'>"{searchQuery}"</span></h1>
@@ -79,18 +86,34 @@ export default function Example() {
             <div className="flex items-center">
               <SortOptions sortOptions={sortOptions} setSortOptions={setSortOptions} />
 
-              <button
+              {/* <button
                 type="button"
                 className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
                 onClick={() => setMobileFiltersOpen(true)}
               >
                 <span className="sr-only">Filters</span>
                 <FunnelIcon className="h-5 w-5" aria-hidden="true" />
+              </button> */}
+              <button
+                type="button"
+                className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+                onClick={handleToggleSidebar}
+              >
+                <span className="sr-only">Filters</span>
+                <FunnelIcon className="h-5 w-5" aria-hidden="true" />
               </button>
-              
-
             </div>
           </div>
+
+          <YourComponent
+            mobileFiltersOpen={mobileFiltersOpen}
+            setMobileFiltersOpen={setMobileFiltersOpen}
+            setValueFilter={setValueFilter}
+            valueFilter={valueFilter}
+            refine={refine}
+            oldCategoryRef={oldCategoryRef}
+            hits={hits}
+          />
 
           <section aria-labelledby="products-heading" className="pb-24 pt-6">
             <div className="xl:justify-start xl:divide-x-2 xl:gap-6 xl:flex lg:flex lg:justify-start lg:divide-x-2 lg:gap-6 md:flex sm:block ">
@@ -145,6 +168,65 @@ function CheckboxLabel({ setValueFilter, valueFilter, refine, oldCategoryRef }) 
 
   );
 }
+
+function YourComponent({ mobileFiltersOpen, setMobileFiltersOpen, setValueFilter, valueFilter, refine, oldCategoryRef, hits }) {
+  return (
+    <Transition show={mobileFiltersOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-40 " onClose={setMobileFiltersOpen}>
+        {/* <Transition.Child
+          as={Fragment}
+          enter="transition-opacity ease-linear duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child> */}
+
+        <div className="fixed inset-0 z-40 flex">
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+              <div className="flex items-center justify-between px-4">
+                <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                <button
+                  type="button"
+                  className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setMobileFiltersOpen(false);
+                  }}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+
+              {/* Filters */}
+              <span className="mt-4 border-t border-gray-200">
+                <SRPriceRange className="pt-6 px-4" />
+                <SRStarRating className="pt-6 px-4" />
+                <CheckboxLabel setValueFilter={setValueFilter} valueFilter={valueFilter} refine={refine} oldCategoryRef={oldCategoryRef} />
+              </span>
+            </Dialog.Panel>
+          </Transition.Child>
+
+
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
+
 
 
 
