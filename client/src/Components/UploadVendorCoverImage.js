faa7e8ff48ab0473431478aaa9b89ae087d8a0e6
ninnh <phoanninhninh@gmail.com>
2023-12-15 19:35:16 +0700

    Create UploadVendorCoverImage

--- client/src/Components/UploadVendorCoverImage.js
+++ client/src/Components/UploadVendorCoverImage.js
@@ -1,7 +1,13 @@
 import { useSearchBox } from "react-instantsearch";
+import { useContext, useState, useEffect, useCallback } from 'react'
 
-export default function VandorNav({ vendor, activeTab, vendorImage }) {
+export default function UploadVendorCoverImage({ vendor, activeTab, vendorImage }) {
   const { refine } = useSearchBox();
+  const [file, setFile] = useState('');
+  const handleFileChange = (event) => {
+    event.preventDefault()
+    setFile(event.target.files[0]);
+  };
 
   const getTabClass = (tabName) => {
     return `border-b-2 px-2 py-3 duration-700 transition ${activeTab === tabName ? 'border-black' : 'border-transparent hover:border-black'
@@ -10,8 +16,42 @@ export default function VandorNav({ vendor, activeTab, vendorImage }) {
   return (
     <>
       {/* Profile Section */}
+
       <div class="md:container mx-auto">
-        <img class="h-auto max-w-full" src="https://m.media-amazon.com/images/S/al-na-9d5791cf-3faf/cd1707cc-cbfa-4ad9-9c64-b92f7bf2dc42._CR0%2C0%2C3000%2C600_SX3000_.jpg" alt="" />
+        
+        <section class="container w-full mx-auto items-center py-4">
+                <div class="max-w-full mx-auto items-center">
+                  <div class="px-4 py-6">
+                    <div id="image-preview" class="max-w-full h-48 p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
+                      <input id="upload" onChange={handleFileChange} type="file" class="hidden" accept="image/*" required/>
+                      <label for="upload" class="cursor-pointer">
+                        {file && (
+                          <div>
+                            <h2 class="font-normal text-sm text-gray-600 md:px-6 " >Image Preview:</h2>
+                            <img
+                              src={URL.createObjectURL(file)}
+                              alt="Preview"
+                              style={{ maxWidth: '100%' }}
+                            />
+                            <p class="font-normal text-sm text-gray-400 md:px-6">File Name: {file.name}</p>
+                          </div>
+                        )}
+                        {!file && (
+                        <div>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                          </svg>
+                          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture</h5>
+                          <p class="font-normal text-sm text-gray-400 md:px-6">You should choose photo size with <b class="text-gray-600">height {">"} 950px</b> and <b class="text-gray-600">width {"<"} 300px</b></p>
+                          <p class="font-normal text-sm text-gray-400 md:px-6">and must be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
+                          <span id="filename" class="text-gray-500 bg-gray-200 z-50"></span>
+                        </div>)}
+                      </label>
+                    </div>
+                  </div>
+                </div>
+        </section>
+
         <div class="md:flex my-3 md:justify-between px-4 md:px-0">
           <div class="flex items-center gap-4">
             <img src={(vendorImage) ? `data:image/jpeg;base64,${vendorImage}` : require("../Components/images/defaultUserImage.png")} className="vendor-avatar md:w- rounded-full" alt="" />
