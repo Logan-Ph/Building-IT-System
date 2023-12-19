31413f0bdacaf5c781f455dfb17db129a2b093a3
navahok164 <s3997024@rmit.edu.vn>
2023-12-20 00:25:11 +0700

    update sidebar

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -90,7 +90,7 @@ export function SidebarItem({ icon, text, active, alert, subitems }) {
         onClick={handleSubitemsToggle}
       >
         {icon}
-        <span className={`overflow-hidden transition-all lg:md:text-lg   ${expanded ? "w-52 ml-3" : "w-0"}`}>{text} </span>
+        <span className={`overflow-hidden transition-all lg:md:text-lg   ${expanded ? "lg:md:w-52 xs:sm:w-40 ml-3" : "w-0"}`}>{text} </span>
         {expanded && (showSubitems ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-up"><path d="m18 15-6-6-6 6" /></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>)}
         {alert && (
           <div
