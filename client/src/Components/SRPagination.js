eb9582a32bba69e762f3566a797dc94c6c49de50
nguyenngocthanhmai <mainguyenngocthanh040426@gmail.com>
2023-12-20 23:47:14 +0700

    pagination update

--- client/src/Components/SRPagination.js
+++ client/src/Components/SRPagination.js
@@ -11,8 +11,8 @@ export default function Example() {
   const previousPageIndex = (currentRefinement - 1) > 0 ? currentRefinement - 1 : 0;
   const nextPageIndex = (currentRefinement + 1) === nbPages ? currentRefinement : currentRefinement + 1;
   return (
-    <div className="flex items-center px-4 py-3 sm:px-6 mt-5">
-      <div className="xs:flex xs:flex-1 xs:justify-end sm:flex sm:flex-1 sm:items-center sm:justify-between lg:justify-end">
+    <div className="flex items-center mt-5">
+      <div className="xs:flex xs:flex-1 xs:justify-end sm:flex sm:flex-1 sm:items-center sm:justify-end lg:justify-end">
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
             <span
