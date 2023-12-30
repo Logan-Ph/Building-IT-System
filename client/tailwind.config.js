00a2c2a048f39eb5639b73905ebdec4f70ffb033
navahok164 <s3997024@rmit.edu.vn>
2023-12-31 01:15:10 +0700

    a

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,8 +23,7 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('flowbite/plugin'),
-    
+    require('flowbite/plugin'),    
      {
       tailwindcss: {},
       autoprefixer: {},
