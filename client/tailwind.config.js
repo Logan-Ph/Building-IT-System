eaf51ca98bc31a361cab357fb83212ab9fe22bae
navahok164 <s3997024@rmit.edu.vn>
2024-01-01 03:13:03 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'false',
+  darkMode: false,
+
   mode: 'jit',
   content: ["./src/**/*.{js,jsx,ts,tsx}"
     , "./src/**/*.{js,jsx,ts,tsx}",
