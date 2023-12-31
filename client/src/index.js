fd36c285406b5150b42c414a4fcbe0791956d6ed
navahok164 <s3997024@rmit.edu.vn>
2024-01-01 02:57:21 +0700

    remove flowbite

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css'
 import './css/style.css'
-import { useEffect } from 'react';
-import { initFlowbite } from 'flowbite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "./src/**/**/*.{js,jsx,ts,tsx}",
     './node_modules/flowbite-react/lib/esm/**/*.js',
     'node_modules/flowbite-react/lib/esm/**/*.js',
-    "./node_modules/flowbite/**/*.js",
   ],
 
   theme: {
