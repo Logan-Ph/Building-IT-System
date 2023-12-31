d98a9bc150afa44e109338643888617b989e8037
navahok164 <s3997024@rmit.edu.vn>
2024-01-01 00:30:53 +0700

    nan ni? a'

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"
     , "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/**/*.{js,jsx,ts,tsx}",
-    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+
 
   ],
 
