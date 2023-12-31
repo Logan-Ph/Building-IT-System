1c59456a5ba9b4547ea9a219142ce4f9f6c328f9
navahok164 <s3997024@rmit.edu.vn>
2024-01-01 02:34:07 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import Router from './Components/Router'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { Flowbite } from 'flowbite-react';
 
 function App() {
   return (
-    <>
-      <Router />
-    </>
+    <Flowbite theme={{ dark: false }}>
+      <>
+        <Router />
+      </>
+    </Flowbite>
+
   );
 }
 export default App;
