e4516fb393e5a91a4f06530485ffe67c3fa2de81
navahok164 <s3997024@rmit.edu.vn>
2023-12-31 23:54:24 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"
     , "./src/**/*.{js,jsx,ts,tsx}",
     "./src/**/**/*.{js,jsx,ts,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
 
   ],
   
