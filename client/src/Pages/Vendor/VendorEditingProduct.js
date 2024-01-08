9f7d72c5c7d7b4d84b544d24fe8d4a9e3a6c9a65
Logan-Ph <phamphuocsang5850@gmail.com>
2024-01-08 15:00:58 +0700

    add bg image for vendor

--- client/src/Pages/Vendor/VendorEditingProduct.js
+++ client/src/Pages/Vendor/VendorEditingProduct.js
@@ -16,7 +16,7 @@ export default function VendorEditPostingProduct() {
   const [msg, setMsg] = useState('')
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext)
 
   const notify = (error) => {
     toast.error(error, {
@@ -97,7 +97,7 @@ export default function VendorEditPostingProduct() {
   }, [error, msg, fetchProduct]);
 
 
-  if (!user){
+  if (!user) {
     return null;
   }
 
@@ -129,7 +129,7 @@ export default function VendorEditPostingProduct() {
                     <div id="image-preview" class="max-w-sm p-6 mb-4 bg-gray-100 border-dashed border-2 border-gray-400 rounded-lg items-center mx-auto text-center cursor-pointer">
                       <input id="upload" onChange={handleFileChange} type="file" class="hidden" accept="image/*" />
                       <label for="upload" class="cursor-pointer">
-                        {file && (
+                        {/* {file && (
                           <div>
                             <h2 class="font-normal text-sm text-gray-600 md:px-6 " >Image Preview:</h2>
                             <img
@@ -139,8 +139,8 @@ export default function VendorEditPostingProduct() {
                             />
                             <p class="font-normal text-sm text-gray-400 md:px-6">File Name: {file.name}</p>
                           </div>
-                        )}
-                        {!file && (
+                        )} */}
+                        {/* {!file && (
                           <div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
                               <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
@@ -149,7 +149,57 @@ export default function VendorEditPostingProduct() {
                             <p class="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be less than <b class="text-gray-600">2mb</b></p>
                             <p class="font-normal text-sm text-gray-400 md:px-6">and should be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
                             <span id="filename" class="text-gray-500 bg-gray-200 z-50"></span>
-                          </div>)}
+                          </div>
+                        )} */}
+                        <div style={file ? { backgroundImage: `url(${URL.createObjectURL(file)})` } : { backgroundImage: `url(${product.image_link})` }}>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-gray-700 mx-auto mb-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                          </svg>
+                          <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-700">Upload picture</h5>
+                          <p class="font-normal text-sm text-gray-400 md:px-6">Choose photo size should be less than <b class="text-gray-600">2mb</b></p>
+                          <p class="font-normal text-sm text-gray-400 md:px-6">and should be in <b class="text-gray-600">JPG, PNG, or GIF</b> format.</p>
+                          <span id="filename" class="text-gray-500 bg-gray-200 z-50"></span>
+                          <input
+                            id="upload"
+                            onChange={handleFileChange}
+                            type="file"
+                            class="hidden"
+                            accept="image/*"
+                            required
+                          />
+                          <label for="upload" class="cursor-pointer">
+                            <div class="font-extrabold tracking-tight leading-none text-gray-700">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="w-8 h-8 mx-auto mb-4"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                                />
+                              </svg>
+                              <h5 class="mb-2 text-xl">Upload product Image</h5>
+                              <p class="font-normal text-sm md:px-6">
+                                You should choose photo size with{" "}
+                                <b class="italic ...">height {">"} 950px</b> and{" "}
+                                <b class="italic ...">width {"<"} 300px</b>
+                              </p>
+                              <p class="font-normal text-sm md:px-6">
+                                and must be in{" "}
+                                <b class="italic ...">JPG, PNG, or GIF</b> format.
+                              </p>
+                              <span
+                                id="filename"
+                                class="text-gray-500 bg-gray-200 z-50"
+                              ></span>
+                            </div>
+                          </label>
+                        </div>
                       </label>
                     </div>
                   </div>
